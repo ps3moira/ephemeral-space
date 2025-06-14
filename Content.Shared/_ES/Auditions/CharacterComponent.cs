2b51c39c9347f062e55c6a90c60f0f79d9cd48c8
Rain Caldwell <rain@is-going-to.cyou>
2025-06-14 22:53:29 +0800

    datafield+networked comp

--- Content.Shared/_ES/Auditions/CharacterComponent.cs
+++ Content.Shared/_ES/Auditions/CharacterComponent.cs
@@ -1,29 +1,30 @@
 using Content.Shared.Humanoid;
 using Robust.Shared.Enums;
+using Robust.Shared.GameStates;
 
 namespace Content.Shared._ES.Auditions;
 
 /// <summary>
 /// This is used for marking the character of components.
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class CharacterComponent : Component
 {
-    [ViewVariables]
+    [ViewVariables, DataField]
     public string Name = "Rain Miskovitch";
 
-    [ViewVariables]
+    [ViewVariables, DataField]
     public int Age = 25;
 
-    [ViewVariables]
+    [ViewVariables, DataField]
     public Gender Gender = Gender.Neuter;
 
-    [ViewVariables]
+    [ViewVariables, DataField]
     public HumanoidCharacterAppearance Appearance = default!;
 
-    [ViewVariables]
+    [ViewVariables, DataField]
     public Dictionary<string, string> Relationships = new ();
 
-    [ViewVariables]
+    [ViewVariables, DataField]
     public List<EntityUid> Memories = new ();
 }
--- Content.Shared/_ES/Auditions/CrewComponent.cs
+++ Content.Shared/_ES/Auditions/CrewComponent.cs
@@ -1,3 +1,4 @@
+using Robust.Shared.GameStates;
 using Robust.Shared.Utility;
 
 namespace Content.Shared._ES.Auditions;
@@ -5,34 +6,36 @@ namespace Content.Shared._ES.Auditions;
 /// <summary>
 /// This is used for documenting crews.
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class CrewComponent : Component
 {
     /// <summary>
     /// Who's the captain of this ship?
     /// </summary>
-    [ViewVariables]
+    [ViewVariables, DataField]
     public EntityUid? Captain = null;
 
     /// <summary>
     /// Who are the crew aboard the ship? This includes the captain.
     /// </summary>
-    [ViewVariables]
+    [ViewVariables, DataField]
     public List<EntityUid> Crew = new ();
 
     /// <summary>
     /// How many souls are aboard this ship? This excludes the captain.
     /// </summary>
-    [ViewVariables]
+    [ViewVariables, DataField]
     public int CrewCount = 5;
 
     /// <summary>
     /// Which ship grid is this crew entity assigned to (if it spawned)?
     /// </summary>
+    [ViewVariables, DataField]
     public EntityUid? ShipGrid = null;
 
     /// <summary>
     /// What map should the system spawn when spawning the crew?
     /// </summary>
+    [ViewVariables, DataField]
     public ResPath MapPath = default!;
 }
