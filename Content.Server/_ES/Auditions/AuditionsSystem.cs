cefef478fd55652e349fdb8fd0286e627d50505f
Rain Caldwell <rain@is-going-to.cyou>
2025-06-14 15:11:51 +0800

    doccomment

--- Content.Server/_ES/Auditions/AuditionsSystem.cs
+++ Content.Server/_ES/Auditions/AuditionsSystem.cs
@@ -3,6 +3,6 @@ using Content.Shared._ES.Auditions;
 namespace Content.Server._ES.Auditions;
 
 /// <summary>
-/// This handles...
+/// This handles the server-side of auditioning!
 /// </summary>
 public sealed class AuditionsSystem : SharedAuditionsSystem;
