c8065a185018e1dc9d502a7e9415bb948532ef90
Rain Caldwell <rain@is-going-to.cyou>
2025-06-15 00:51:40 +0800

    make the api better

--- Content.Server/_ES/Auditions/AuditionsSystem.cs
+++ Content.Server/_ES/Auditions/AuditionsSystem.cs
@@ -14,39 +14,51 @@ public sealed class AuditionsSystem : SharedAuditionsSystem
     [Dependency] private readonly IRobustRandom _random = default!;
 
     /// <summary>
+    /// Generates and integrates a crew.
+    /// </summary>
+    public Entity<CrewComponent> GenerateAndIntegrateCrew(int crewCount = 10, ProducerComponent? producer = null)
+    {
+        if (!TryGetProducer(ref producer))
+            throw new Exception("Could not get ProducerComponent!");
+
+        var crew = GenerateRandomCrew(crewCount);
+        if (!crew.Comp.Captain.HasValue)
+            throw new Exception("Crew did not have a captain upon assignment");
+
+        return crew;
+    }
+
+    /// <summary>
     /// Hires a cast, and integrates relationships between all of the characters.
     /// </summary>
     public void GenerateCast(
         int captainCount = 26,
-        int minimumCrew = 5,
-        int maximumCrew = 12,
-        [ForbidLiteral] string lowPopPoolPrototype = "DefaultShipPoolLowPop",
-        [ForbidLiteral] string highPopPoolPrototype = "DefaultShipPoolHighPop",
+        int crewCount = 10,
         ProducerComponent? producer = null
     )
     {
         if (!TryGetProducer(ref producer))
             throw new Exception("Could not get ProducerComponent!");
 
-        var lowPopPool = _prototypeManager.Index<GameMapPoolPrototype>(lowPopPoolPrototype);
-        var highPopPool = _prototypeManager.Index<GameMapPoolPrototype>(highPopPoolPrototype);
-
         var captains = new List<Entity<CharacterComponent>>();
 
         for (var i = 0; i < captainCount; i++)
         {
-            var crewCount = _random.Next(minimumCrew, maximumCrew);
-            var lowPop = crewCount < maximumCrew / 1.5;
-            var map = lowPop ? _random.Pick(lowPopPool.Maps) : _random.Pick(highPopPool.Maps);
-            var gameMapPrototype = _prototypeManager.Index<GameMapPrototype>(map);
-
-            var crew = GenerateRandomCrew(crewCount, gameMapPrototype.MapPath);
-            if (!crew.Comp.Captain.HasValue)
-                throw new Exception("Crew did not have a captain upon assignment");
-            captains.Add((crew.Comp.Captain.Value, EnsureComp<CharacterComponent>(crew.Comp.Captain.Value)));
+            var newCrew = GenerateAndIntegrateCrew(crewCount, producer);
+            captains.Add((newCrew.Comp.Captain!.Value, EnsureComp<CharacterComponent>(newCrew.Comp.Captain.Value)));
         }
 
         IntegrateRelationshipGroup(producer.CaptainContext, captains);
         IntegrateRelationshipGroup(producer.IntercrewContext, producer.Characters);
+    }
+
+    public void GenerateCast(
+        int captainCount = 26,
+        int minimumCrew = 5,
+        int maximumCrew = 12,
+        ProducerComponent? producer = null
+    )
+    {
+        GenerateCast(captainCount, _random.Next(minimumCrew, maximumCrew), producer);
     }
 }
