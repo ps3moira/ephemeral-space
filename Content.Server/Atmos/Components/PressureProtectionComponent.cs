9f1193816e1ccea8db6a8ddf7cc14f99cf0bd90f
Kevin Zheng <kevinz5000@gmail.com>
2025-07-19 21:05:20 -0700

    Implement puncture damage in pressure-protecting suits

--- Content.Server/Atmos/Components/PressureProtectionComponent.cs
+++ Content.Server/Atmos/Components/PressureProtectionComponent.cs
@@ -17,6 +17,13 @@ public sealed partial class PressureProtectionComponent : Component
 
     [DataField]
     public float LowPressureModifier;
+
+    /// <summary>
+    /// If the entity is also Damageable, the total damage that can be dealt before this pressure protection is
+    /// compromised.
+    /// </summary>
+    [DataField]
+    public float CompromisedDamageThreshold = 50f;
 }
 
 /// <summary>
